ce0b7eaa53912603206ecce44bd9fddd44aebd19
meowkun <ktao1@umbc.edu>
2021-02-24 18:52:48 -0500

    .gitignore is now working

---
+++ 2D/Number Wizard Game/Assets/Script/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+
+    [SerializeField]
+    int max;
+    [SerializeField]
+    int min;
+    [SerializeField]
+    Text guessText;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void heigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void lower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        max += 1;
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ 2D/Number Wizard Game/Assets/Script/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // get current scene index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // move to next scene
+        if (currentSceneIndex < 2)
+        {
+            SceneManager.LoadScene(currentSceneIndex += 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+            
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
